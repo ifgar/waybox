57c45c6d10e5d6302286cdeeaac57fa1f9011677
ignaciofgar <ignaciofgar@proton.me>
2025-12-06 18:56:20 +0100

    add Options class

---
+++ lib/core/options.dart
@@ -0,0 +1,16 @@
+class Options {
+  final double? width;
+  final double? height;
+
+  final String? primary;
+  final String? secondary;
+  final String? background;
+
+  const Options({
+    this.width,
+    this.height,
+    this.primary,
+    this.secondary,
+    this.background,
+  });
+}

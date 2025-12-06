ea833f31b28cd3dd8cae15d00edf5f4f0b47ff1e
ignaciofgar <ignaciofgar@proton.me>
2025-12-06 17:31:42 +0100

    define parseMenu for parsing XML file

--- lib/core/menu_parser.dart
+++ lib/core/menu_parser.dart
@@ -1,6 +1,16 @@
 import 'package:waybox/core/menu.dart';
 import 'package:xml/xml.dart';
 
-Menu parseMenu(XmlElement element){
-  
+Menu parseMenu(XmlElement element) {
+  final name = element.getAttribute("name") ?? "undefined";
+
+  final command = element.getAttribute("command");
+
+  final children = element.findElements("menu").map(parseMenu).toList();
+
+  return Menu(
+    name: name,
+    command: command,
+    children: children.isEmpty ? null : children,
+  );
 }
